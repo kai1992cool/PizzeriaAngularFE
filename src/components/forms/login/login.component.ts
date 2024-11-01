09d4d28d16269e0908c21d52fe1ee00f2e5bb425
Claudius10 <66148715+Claudius10@users.noreply.github.com>
2024-11-01 16:37:13 +0100

    implemented functional login component

--- src/components/forms/login/login.component.ts
+++ src/components/forms/login/login.component.ts
@@ -1,16 +1,58 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, DestroyRef, inject} from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {PaginatorModule} from 'primeng/paginator';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {emailRgx, passwordRegex} from '../../../regex';
+import {LoginService} from '../../../services/login/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    PaginatorModule,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent {
+  private loginService = inject(LoginService);
+  private destroyRef = inject(DestroyRef);
 
+  form = new FormGroup({
+    email: new FormControl<string>("", {
+      validators: [Validators.pattern(emailRgx)],
+      nonNullable: true,
+      updateOn: 'blur'
+    }),
+    password: new FormControl<string>("", {
+      validators: [Validators.pattern(passwordRegex)],
+      nonNullable: true,
+      updateOn: 'blur'
+    }),
+  });
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      console.log(this.form.controls);
+      return;
+    }
+
+    const sub = this.loginService.login({
+      email: this.form.get("email")!.value,
+      password: this.form.get("password")!.value,
+    }).subscribe({
+      next: value => {
+        console.log("response: ", value);
+      },
+      error: error => {
+        console.log("error: ", error.error);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => sub.unsubscribe());
+  }
 }
--- src/services/login/login.service.ts
+++ src/services/login/login.service.ts
@@ -1,5 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {LoginForm} from '../../interfaces/dto/forms/account';
+import {catchError} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,14 @@ import {HttpClient} from '@angular/common/http';
 export class LoginService {
   private httpClient = inject(HttpClient);
 
-
+  public login(data: LoginForm) {
+    return this.httpClient.post(`http://192.168.1.128:8080/api/auth/login?username=${data.email}&password=${data.password}`,
+      {responseType: "text"},
+      {withCredentials: true})
+      .pipe(
+        catchError((err) => {
+          throw err as string;
+        })
+      );
+  }
 }
